d5bf6ad2c3700daea1fdb0ad3941575e89203847
Dane Cronin <danecronin@gmail.com>
2025-01-04 19:23:02 -0700

    added scaffolding for remaining project case-study pages

--- src/pages/Home/projects.js
+++ src/pages/Home/projects.js
@@ -58,28 +58,9 @@ export const projects = [
     tags: ["Creative Campaign, Web Development, Interactive Design, ECommerce"],
     date: "October 2024",
     projectLink: "/work/2", // Use a unique route for each project
-    challenge: `Cliffhanger is a tribute to the interplay between light and
-                form. During a time when the world seemed paused, we set out to
-                create a piece that celebrates movement, space, and emotion.
-                Through thoughtful creative direction and intricate
-                post-production, we crafted a narrative that feels both intimate
-                and expansive—a beacon for those searching for connection and
-                inspiration in uncertain times.`,
-    challengeDesc: `Cliffhanger explores the idea of finding clarity in moments of
-                obscurity. Inspired by the duality of light and shadow, we
-                imagined a character who navigates a surreal world, guided by
-                beams of light that reveal hidden paths and truths. This
-                character, free from physical constraints, embodies the
-                liberation from everyday burdens, moving fluidly through spaces
-                that are both familiar and otherworldly.`,
-    creativeSolution: `To bring Cliffhanger to life, we focused on creating a
-                seamless blend of reality and surrealism. Escapism became our
-                guiding principle, leading us to juxtapose real-life moments
-                with ethereal 3D landscapes. Shot on the quiet streets of
-                Saigon, our Director of Photography, Ray Lavers, captured
-                fleeting glimpses of reality in just two hours during
-                quarantine. These grounded visuals set the stage for the
-                dreamlike sequences that followed.`,
+    challenge: `Design an inspiring and interactive Ecommerce web store for a boutique coffee store in Boulder, CO.`,
+    challengeDesc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+    creativeSolution: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
     image1: "/marquee/DustToDust.jpg",
     image2: "/marquee/GreenRiver.jpg",
     role: "Web Development, ECommerce, UX/UI, Interactive Design",
@@ -97,14 +78,34 @@ export const projects = [
     description: "Creative Campaign, Web Development, Interactive Design, ECommerce"
   },
 
-
   {
-    id: 3,
-    title: "Taliesin West",
-    description: "Architecture Design, Editorial, Photography",
-    image: "/projects/Taliesin-2.jpg",
+    id: 3, // Unique identifier for each project
     column: 1,
+    title: "Taliesin West",
+    name: "Taliesin West",
+    bannerImage: "/projects/Taliesin-2.jpg",
+    tags: ["Architecture Design, Editorial, Photography"],
+    date: "Dec 2024",
+    projectLink: "/work/3", // Use a unique route for each project
+    challenge: `Create stunning imagery documenting the mid-century modern project "Taliesin West" by legendary architect Frank Lloyd Wright`,
+    challengeDesc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+    creativeSolution: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+    image1: "/marquee/Taliesin-3.jpg",
+    image2: "/marquee/Taliesin-4.jpg",
+    role: "Architecture Design, Editorial, Photography",
+    credits: [
+      { role: "Project", name: "Taliesin West" },
+      { role: "Photographer", name: "Dane Cronin (neer&cronin)" },
+      { role: "Creative Producer", name: "Dane Cronin" },
+      { role: "Art Direction", name: "Dane Cronin" },
+      { role: "Architectural Design", name: "Frank Lloyd Wright" },
+    
+      { role: "Acknowledgments", name: "Taliesin West, Dane Cronin" },
+    ],
+    image: "/projects/Taliesin-2.jpg", // Thumbnail image for the project
+    description: "Architecture Design, Editorial, Photography"
   },
+
 
   {
     id: 4, // Unique identifier for each project
@@ -116,8 +117,8 @@ export const projects = [
     date: "April 2023",
     projectLink: "/work/4", // Use a unique route for each project
     challenge: "Cafe Americano is a tribute to the interplay between light and form...",
-    challengeDesc: "Cafe Americano explores the idea of finding clarity in moments of obscurity...",
-    creativeSolution: "To bring Cafe Americano to life, we focused on creating a seamless blend of reality and surrealism...",
+    challengeDesc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+    creativeSolution: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
     image1: "/marquee/CafeAmericano.jpg",
     image2: "/marquee/GreenRiver.jpg",
     role:  "Motion Capture, Creative Direction, Post-Production, Color-Grading",
@@ -137,34 +138,126 @@ export const projects = [
     description: "Experimental Film, Creative Campaign, Film Production"
   },
 
+
   {
-    id: 5,
+    id: 5, // Unique identifier for each project
+    column: 2,
     title: "Good Company",
-    description: "Film Production, Creative Campaign",
-    image: "/projects/GoodCompany.jpg",
-    column: 2,
+    name: "Good Company",
+    bannerImage: "/projects/GoodCompany.jpg",
+    tags: ["Film Production", "Creative Direction", "Creative Campaign"],
+    date: "May 2022",
+    projectLink: "/work/5", // Use a unique route for each project
+    challenge: "Good Company...",
+    challengeDesc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+    creativeSolution: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+    image1: "/marquee/CafeAmericano.jpg",
+    image2: "/marquee/GreenRiver.jpg",
+    role:  "Film Production, Creative Campaign",
+    credits: [
+      { role: "Project", name: "Good Company" },
+      { role: "Directors", name: "Shawn Neer" },
+      { role: "Creative Producer", name: "Shawn Neer" },
+      { role: "Art Direction & Animation", name: "Shawn Neer (neer&cronin)" },
+      { role: "Editing & Compositing", name: "Craig Grant" },
+      { role: "Cinematography & Color Grading", name: "Craig Grant" },
+  
+      { role: "Sound Design", name: "Echo Chamber Studios" },
+      { role: "Label", name: "Moniker Productions" },
+      { role: "Acknowledgments", name: "Yeti Cycles, Shawn Neer, Jubal Davis, Craig Grant" },
+    ],
+    image: "/projects/GoodCompany.jpg", // Thumbnail image for the project
+    description: "Film Production, Creative Campaign"
   },
+
   {
-    id: 6,
+    id: 6, // Unique identifier for each project
+    column: 2,
     title: "DUST TO DUST",
-    description: "Creative Campaign, Color Grading, Film Production",
-    image: "/projects/DustToDust.jpg",
-    column: 2,
+    name: "DUST TO DUST",
+    bannerImage: "/projects/DustToDust.jpg",
+    tags: ["Creative Campaign, Color Grading, Film Production"],
+    date: "May 2021",
+    projectLink: "/work/6", // Use a unique route for each project
+    challenge: "Dust to Dust...",
+    challengeDesc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+    creativeSolution: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+    image1: "/marquee/CafeAmericano.jpg",
+    image2: "/marquee/GreenRiver.jpg",
+    role:  "Creative Campaign, Color Grading, Film Production",
+    credits: [
+      { role: "Project", name: "Dust To Dust" },
+      { role: "Directors", name: "Shawn Neer" },
+      { role: "Creative Producer", name: "Shawn Neer" },
+      { role: "Art Direction & Animation", name: "Shawn Neer (neer&cronin)" },
+      { role: "Editing & Compositing", name: "Craig Grant" },
+      { role: "Cinematography & Color Grading", name: "Craig Grant" },
+  
+      {  },
+      { role: "Label", name: "Moniker Productions" },
+      { role: "Acknowledgments", name: "Yeti Cycles, Shawn Neer, Adolf Silva, Craig Grant" },
+    ],
+    image: "/projects/DustToDust.jpg", // Thumbnail image for the project
+    description: "Creative Campaign, Color Grading, Film Production"
   },
+
   {
-    id: 7,
+    id: 7, // Unique identifier for each project
+    column: 2,
     title: "GUAVA Family",
-    description:
-      "Commercial Photography, Lifestyle, Photography Production",
-    image: "/projects/Guava-1.jpg",
-    column: 2,
+    name: "GUAVA Family",
+    bannerImage: "/projects/Guava-1.jpg",
+    tags: ["Commercial Photography, Lifestyle, Photography Production"],
+    date: "June 2022",
+    projectLink: "/work/7", // Use a unique route for each project
+    challenge: `Create stunning imagery for the Guava Family brand featuring runner Neely Gracey in advance of a World Record Attempt`,
+    challengeDesc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+    creativeSolution: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+    image1: "/marquee/Guava-2.jpg",
+    image2: "/marquee/Guava-2.jpg",
+    role: "Commercial Photography, Lifestyle, Photography Production",
+    credits: [
+      { role: "Project", name: "GUAVA Family World Record Attempt" },
+      { role: "Photographer", name: "Dane Cronin (neer&cronin)" },
+      { role: "Creative Producer", name: "Dane Cronin" },
+      { role: "Art Direction", name: "Reed @Guava Family" },
+      { role: "Athlete", name: "Neely Gracey" },
+    
+      { role: "Acknowledgments", name: "Guava Family, Dane Cronin, Neely Gracey" },
+    ],
+    image: "/projects/Guava-1.jpg", // Thumbnail image for the project
+    description: "Commercial Photography, Lifestyle, Photography Production"
   },
+
+
+
   {
-    id: 8,
-    title: "Espresso Forge",
-    description: "Web Design, ECommerce, UX/UI",
-    image: "/projects/project8.jpg",
+    id: 8, // Unique identifier for each project
     column: 2,
+    title: "Espresso Forge",
+    name: "Espresso Forge",
+    bannerImage: "/projects/project8.jpg",
+    tags: ["Web Design, ECommerce, UX/UI"],
+    date: "June 2021",
+    projectLink: "/work/8", // Use a unique route for each project
+    challenge: `Design a fully custom and integrated ECommerce website for Espresso Forge.`,
+    challengeDesc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+    creativeSolution: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+    image1: "/projects/project8.jpg",
+    image2: "/marquee/GreenRiver.jpg",
+    role: "Web Design, ECommerce, UX/UI",
+    credits: [
+      { role: "Project", name: "Espresso Forge" },
+      { role: "Site Design", name: "Shawn Neer (neer&cronin)" },
+      { role: "Creative Producer", name: "Shawn Neer" },
+      { role: "Art Direction & Animation", name: "Shawn Neer (neer&cronin)" },
+      { role: "Web Development", name: "Shawn Neer" },
+      { role: "E-Commerce", name: "Shawn Neer" },
+      { role: "UX/UI Design", name: "Shawn Neer" },
+      { role: "Acknowledgments", name: "Espresso Forge, Shawn Neer" },
+    ],
+    image: "/projects/project8.jpg", // Thumbnail image for the project
+    description: "Creative Campaign, Web Development, Interactive Design, ECommerce"
   },
 ];
 
